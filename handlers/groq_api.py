199b401a6165af02ee3c555bc90962bd6242b845
lumino <luminovoez@gmail.com>
2024-05-03 14:24:18 +0800

    prompt

--- handlers/groq_api.py
+++ handlers/groq_api.py
@@ -5,6 +5,9 @@ from groq import Groq
 load_dotenv()
 
 client = Groq(api_key=os.getenv("API_KEY"))
+system_prompt = os.getenv("SYSTEM_PROMPT")
+print(system_prompt)
+
 
 def api(data: str) -> str:
   content = data
@@ -13,7 +16,7 @@ def api(data: str) -> str:
     messages=[
       {
         "role": "system",
-        "content": "请用中文回答我下面的问题。回答前明确将要回答的内容，回答时请尽可能地详细。如果回答的是英文，请将英文翻译成中文。请一定要用中文回答。",
+        "content": system_prompt,
       },
       {"role": "user", "content": content},
     ],
